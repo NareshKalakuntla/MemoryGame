3eec8e467c01f507b3dcce174e596097f7d35a0b
Naresh Kalakuntla <naresh.kalakuntla90@gmail.com>
2016-01-14 01:52:35 +0530

    Initial Commit

---
+++ Memory Game/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memory Game
+//
+//  Created by Naresh Kalakuntla on 1/14/16.
+//  Copyright © 2016 Naresh Kalakuntla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Memory Game/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Memory Game
+//
+//  Created by Naresh Kalakuntla on 1/14/16.
+//  Copyright © 2016 Naresh Kalakuntla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
